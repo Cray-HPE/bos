6095408e88a702710cd22e21aa2525c8ab59eb74
Mitch Harding <mitchell.harding@hpe.com>
2021-05-10 17:47:58 -0500

    CASMCMS-7042: CASMCMS-7062: Add MIT license to source files; Simplify versioning; Update README; Add copyright/license linter

--- runLint.sh
+++ runLint.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 
+# Copyright 2019, 2021 Hewlett Packard Enterprise Development LP
 #
-# Copyright 2019, Cray Inc.  All Rights Reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# (MIT License)
 
-# Very simple scanner for files missing copyrights
+# Very simple scanner for files missing copyrights & licenses
 
 # Extensions to check
-CODE_EXTENSIONS="py sh"
+CODE_EXTENSIONS="py sh spec"
 
 # Additional files to check, uses exact match
-EXTRA_FILES="Dockerfile api/openapi.yaml"
+EXTRA_FILES="Dockerfile run_codestylecheck run_unittests"
 
-WHITELIST_FILES="src/server/bos/models/boot_set.py
-src/server/bos/models/link.py
-src/server/bos/models/problem_details.py
-src/server/bos/models/session.py
-src/server/bos/models/session_template.py
-src/server/bos/models/version.py
-src/server/README.md
-src/server/test-requirements.txt
-src/server/requirements.txt
-src/server/bos/util.py
-src/server/bos/typing_utils.py
-src/server/bos/models/__init__.py
-src/server/bos/models/base_model_.py
-src/server/bos/openapi/openapi.yaml
-src/server/.gitignore
-src/server/Dockerfile
-src/server/.dockerignore
-src/server/setup.py
-src/server/tox.ini
-src/server/git_push.sh
-src/server/.travis.yml
-src/server/bos/encoder.py
-src/server/bos/test/__init__.py
-src/server/bos/__init__.py
-src/server/.openapi-generator/VERSION"
+WHITELIST_FILES=""
 
 FAIL=0
 
@@ -47,6 +41,11 @@ function scan_file {
         grep -q "Copyright" $1
         if [ $? -ne 0 ]; then
             echo "missing copyright headers"
+            return 1
+        fi
+        grep -q "MIT License" $1
+        if [ $? -ne 0 ]; then
+            echo "missing MIT license"
             return 1
         fi
     fi
@@ -66,16 +65,13 @@ function list_include_item {
   return $result
 }
 
-# Scan extentions
+# Scan extensions
 for CE in ${CODE_EXTENSIONS}
 do
     for F in `git ls-files "*.${CE}"`
     do
         if ! list_include_item "$WHITELIST_FILES" "$F"; then
-            scan_file ${F}
-            if [ $? -ne 0 ]; then
-                FAIL=1
-            fi
+            scan_file ${F} || FAIL=1
         fi
     done
 done
@@ -83,13 +79,15 @@ done
 # Do the listed extra files
 for F in ${EXTRA_FILES}
 do
-    scan_file ${F}
+    if ! list_include_item "$WHITELIST_FILES" "$F"; then
+        scan_file ${F} || FAIL=1
+    fi
 done
 
 if [ ${FAIL} -eq 0 ]; then
     echo "All scanned code passed"
 else
-    echo "Some code is missing copyright, see list above"
+    echo "Some code is missing copyright or license, see list above"
 fi
 
 exit ${FAIL}
