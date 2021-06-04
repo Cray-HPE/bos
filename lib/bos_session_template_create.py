05ba8497bcf90798fa1676e51629cf418e1c5539
Mitch Harding <mitchell.harding@hpe.com>
2021-06-03 21:57:06 -0500

    Use env to find python in bos_session_template_create.py
    (cherry picked from commit d62ce2f3e7f5fc93cb31a9ae1d96732e2444fdc9)

--- lib/bos_session_template_create.py
+++ lib/bos_session_template_create.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright 2019, 2021 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
