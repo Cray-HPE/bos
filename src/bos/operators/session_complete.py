38a341a5106cc283ad8b071162014d9d365942bc
Ryan Bak <rbak@cray.com>
2021-12-01 13:50:41 -0700

    CASMCMS-7185: Add initial session monitoring agent

---
+++ src/bos/operators/session_complete.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Copyright 2021 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# (MIT License)
+
+import logging
+
+from bos.operators.base import BaseOperator, main
+from bos.operators.utils.clients.bos.components import get_components
+from bos.operators.utils.clients.bos.sessions import get_sessions, update_session
+
+LOGGER = logging.getLogger('bos.operators.session_complete')
+
+
+class SessionCompleteOperator(BaseOperator):
+    """
+    The Session Complete Operator marks sessions complete when all components
+    that are part of the session have been disabled.
+    """
+
+    @property
+    def name(self):
+        return 'SessionComplete'
+
+    def _run(self) -> None:
+        """ A single pass of complete sessions """
+        sessions = self._get_incomplete_sessions()
+        for session in sessions:
+            components = self._get_incomplete_components(session)
+            if not components:
+                self._mark_session_complete(session)
+
+    @staticmethod
+    def _get_incomplete_sessions():
+        return get_sessions(complete=False)
+
+    @staticmethod
+    def _get_incomplete_components(session_id):
+        return get_components(session=session_id, enabled=True)
+
+    @staticmethod
+    def _mark_session_complete(session_id):
+        update_session(session_id, {'complete': True})
+        LOGGER.info('Session {} is complete'.format(session_id))
+
+
+if __name__ == '__main__':
+    main(SessionCompleteOperator)
---
+++ src/bos/operators/utils/clients/bos/sessions.py
@@ -0,0 +1,111 @@
+# Copyright 2021 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# (MIT License)
+
+import json
+import logging
+from requests.exceptions import HTTPError, ConnectionError
+from urllib3.exceptions import MaxRetryError
+
+from bos.operators.utils import requests_retry_session
+from bos.operators.utils.clients.bos import ENDPOINT as BASE_ENDPOINT
+
+LOGGER = logging.getLogger('bos.operators.utils.clients.bos.sessions')
+ENDPOINT = "%s/%s" % (BASE_ENDPOINT, __name__.lower().split('.')[-1])
+
+
+def get_session(session_id):
+    """Get information for a single BOS session"""
+    url = ENDPOINT + '/' + session_id
+    session = requests_retry_session()
+    try:
+        response = session.get(url)
+        response.raise_for_status()
+        session = json.loads(response.text)
+    except (ConnectionError, MaxRetryError) as e:
+        LOGGER.error("Unable to connect to BOS: {}".format(e))
+        raise e
+    except HTTPError as e:
+        LOGGER.error("Unexpected response from BOS: {}".format(e))
+        raise e
+    except json.JSONDecodeError as e:
+        LOGGER.error("Non-JSON response from BOS: {}".format(e))
+        raise e
+    return session
+
+
+def get_sessions(**kwargs):
+    """Get information for all BOS sessions"""
+    url = ENDPOINT
+    session = requests_retry_session()
+    try:
+        response = session.get(url, params=kwargs)
+        response.raise_for_status()
+        sessions = json.loads(response.text)
+    except (ConnectionError, MaxRetryError) as e:
+        LOGGER.error("Unable to connect to BOS: {}".format(e))
+        raise e
+    except HTTPError as e:
+        LOGGER.error("Unexpected response from BOS: {}".format(e))
+        raise e
+    except json.JSONDecodeError as e:
+        LOGGER.error("Non-JSON response from BOS: {}".format(e))
+        raise e
+    return sessions
+
+
+def update_session(session_id, data):
+    """Update information for a single BOS session"""
+    url = ENDPOINT + '/' + session_id
+    session = requests_retry_session()
+    try:
+        response = session.patch(url, json=data)
+        response.raise_for_status()
+        session = json.loads(response.text)
+    except (ConnectionError, MaxRetryError) as e:
+        LOGGER.error("Unable to connect to BOS: {}".format(e))
+        raise e
+    except HTTPError as e:
+        LOGGER.error("Unexpected response from BOS: {}".format(e))
+        raise e
+    except json.JSONDecodeError as e:
+        LOGGER.error("Non-JSON response from BOS: {}".format(e))
+        raise e
+    return session
+
+
+def update_sessions(data):
+    """Update information for a multiple BOS sessions"""
+    session = requests_retry_session()
+    try:
+        response = session.patch(ENDPOINT, json=data)
+        response.raise_for_status()
+        session = json.loads(response.text)
+    except (ConnectionError, MaxRetryError) as e:
+        LOGGER.error("Unable to connect to BOS: {}".format(e))
+        raise e
+    except HTTPError as e:
+        LOGGER.error("Unexpected response from BOS: {}".format(e))
+        raise e
+    except json.JSONDecodeError as e:
+        LOGGER.error("Non-JSON response from BOS: {}".format(e))
+        raise e
+    return session
