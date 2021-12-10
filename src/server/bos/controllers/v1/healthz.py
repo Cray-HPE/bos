da6bf5516f3af19c65d1e1baa555a7d3735c4fde
Jason Sollom <jason.sollom@hpe.com>
2021-12-10 17:15:39 -0600

    CASMCMS-7705: Correcting Healthz import

--- src/server/bos/controllers/v1/healthz.py
+++ src/server/bos/controllers/v1/healthz.py
@@ -24,7 +24,7 @@
 import etcd3
 import logging
 
-from bos.models.v1_healthz import V1Healthz as Healthz
+from bos.models.healthz import Healthz as Healthz
 from bos.dbclient import BosEtcdClient
 
 LOGGER = logging.getLogger('bos.controllers.v1.healthz')
