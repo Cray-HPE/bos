03d16913e79ef5f8141aba4d6d193f199be87630
Jason Sollom <jason.sollom@hpe.com>
2022-05-05 11:27:42 -0500

    CASMCMS-7893: CFS uses camel case
    CFS uses camel case in its component entries, but BOS was assuming
    snake case, so it was not detecting the desired configuration correctly.

--- src/bos/operators/filters/filters.py
+++ src/bos/operators/filters/filters.py
@@ -180,9 +180,9 @@ class DesiredConfigurationSetInCFS(LocalFilter):
         # which sets/updates the cfs_components_dict attribute.
         desired_configuration = component.get('desired_state', {}).get('configuration')
         if cfs_component is None:
-            set_configuration = self.cfs_components_dict[component['id']].get('desired_config')
+            set_configuration = self.cfs_components_dict[component['id']].get('desiredConfig')
         else:
-            set_configuration = cfs_component.get('desired_config')
+            set_configuration = cfs_component.get('desiredConfig')
         return desired_configuration == set_configuration
 
 
