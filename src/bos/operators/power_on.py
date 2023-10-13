42bd44984e70fc7c154a277e90467440198a0aa4
Jason Sollom <jason.sollom@hpe.com>
2023-10-13 16:52:44 -0500

    CASMCMS-8835: Do not prematurely filter out disabled nodes
    To truly allow the 'include_disabled' nodes capability, the power
    operators cannot filter out the disabled nodes. This mod removes that
    filter. The session's 'include_disabled' nodes attribute will be used
    in the session setup operator to decide whether the nodes will be
    enabled in BOS or not. If disabled nodes are not included, then they
    will never be enabled and never acted upon. Thus, including them in
    the query to HSM will not hurt anything because they will never be
    acted upon.
    
    When filtering nodes by architecture, do not also filter by whether the
    nodes are enabled in HSM. This is done at a later step in the process.
    Doing this earlier thwarts the later filtering.
    
    (cherry picked from commit a6e9380d98c69a9ed6e801eee30d21adef8c4b59)

--- src/bos/operators/power_on.py
+++ src/bos/operators/power_on.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2021-2023 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2021-2024 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -56,7 +56,7 @@ class PowerOnOperator(PowerOperatorBase):
     def filters(self):
         return [
             BOSQuery(enabled=True, status=Status.power_on_pending),
-            HSMState(enabled=True)
+            HSMState()
         ]
 
     def _act(self, components):
