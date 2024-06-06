b9a33adad0b56d638c2098cd263c73c99870c247
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-06-06 16:33:54 -0400

    Correct errors in function docstrings
    (cherry picked from commit 6ad3b52853b2a24bb85087a0aa73541d01f909ea)

--- src/bos/server/controllers/v2/sessiontemplates.py
+++ src/bos/server/controllers/v2/sessiontemplates.py
@@ -99,9 +99,7 @@ def put_v2_sessiontemplate(session_template_id):  # noqa: E501
     try:
         # Convert the JSON request data into a SessionTemplate object.
         # Any exceptions caught here would be generated from the model
-        # (i.e. bos.server.models.session_template).
-        # An example is an exception for a session template name that
-        # does not conform to Kubernetes naming convention.
+        # (i.e. bos.server.models.v2_session_template).
         # In this case return 400 with a description of the specific error.
         SessionTemplate.from_dict(template_data)
     except Exception as err:
@@ -212,9 +210,7 @@ def patch_v2_sessiontemplate(session_template_id):
     try:
         # Convert the JSON request data into a SessionTemplate object.
         # Any exceptions caught here would be generated from the model
-        # (i.e. bos.server.models.session_template).
-        # An example is an exception for a session template name that
-        # does not confirm to Kubernetes naming convention.
+        # (i.e. bos.server.models.v2_session_template).
         # In this case return 400 with a description of the specific error.
         SessionTemplate.from_dict(template_data)
     except Exception as err:
