5cf124c7c9757910e47342610520c33779c7d172
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-07-15 16:25:16 -0400

    CASMCMS-9026: Sanitize session templates during migration

--- src/bos/server/controllers/v2/sessiontemplates.py
+++ src/bos/server/controllers/v2/sessiontemplates.py
@@ -27,9 +27,8 @@ import connexion
 from bos.common.tenant_utils import get_tenant_from_header, get_tenant_aware_key, \
                                     reject_invalid_tenant
 from bos.common.utils import exc_type_msg
-from bos.server.models.v2_session_template import V2SessionTemplate as SessionTemplate # noqa: E501
 from bos.server import redis_db_utils as dbutils
-from bos.server.utils import _canonize_xname, _get_request_json, ParsingException
+from bos.server.utils import _get_request_json, _validate_sanitize_session_template
 from .boot_set import validate_boot_sets
 
 LOGGER = logging.getLogger('bos.server.controllers.v2.sessiontemplates')
@@ -53,57 +52,6 @@ EXAMPLE_SESSION_TEMPLATE = {
         "configuration": "default-sessiontemplate-cfs-config"},
     "enable_cfs": True,
     "name": "name-your-template"}
-
-
-def _sanitize_xnames(st_json):
-    """
-    Sanitize xnames - Canonize the xnames
-    Args:
-      st_json (dict): The Session Template as a JSON object
-
-    Returns:
-      Nothing
-    """
-    # There should always be a boot_sets field -- this function
-    # is only called after the template has been verified
-    for boot_set in st_json['boot_sets'].values():
-        if 'node_list' not in boot_set:
-            continue
-        boot_set['node_list'] = [_canonize_xname(node) for node in boot_set['node_list']]
-
-
-def _validate_sanitize_session_template(session_template_id, template_data):
-    """
-    Used when creating or patching session templates
-    """
-    # The boot_sets field is required.
-    if "boot_sets" not in template_data:
-            raise ParsingException("Missing required 'boot_sets' field")
-
-    # All keys in the boot_sets mapping must match the 'name' fields in the
-    # boot sets to which they map (if they contain a 'name' field).
-    for bs_name, bs in template_data["boot_sets"].items():
-        if "name" not in bs:
-            # Set the field here -- this allows the name to be validated
-            # per the schema later
-            bs["name"] = bs_name
-        elif bs["name"] != bs_name:
-            raise ParsingException(f"boot_sets key ({bs_name}) does not match 'name' "
-                                   f"field of corresponding boot set ({bs["name"]})")
-
-    # Convert the JSON request data into a SessionTemplate object.
-    # Any exceptions raised here would be generated from the model
-    # (i.e. bos.server.models.v2_session_template).
-    SessionTemplate.from_dict(template_data)
-
-    # We do not bother storing the boot set names inside the boot sets, so delete them.
-    # We know every boot set has a name field because we verified that earlier.
-    for bs in template_data["boot_sets"].values():
-        del bs["name"]
-
-    _sanitize_xnames(template_data)
-    template_data['name'] = session_template_id
-    return
 
 
 @reject_invalid_tenant
