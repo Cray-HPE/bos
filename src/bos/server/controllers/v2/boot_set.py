18d57c9630214fc62ee3c9d53b0d260b48c335a6
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-10-04 14:56:20 -0400

    CASMCMS-9145: Refactor boot image metadata validation logic

--- src/bos/server/controllers/v2/boot_set.py
+++ src/bos/server/controllers/v2/boot_set.py
@@ -24,10 +24,11 @@
 
 from functools import partial
 import logging
-from bos.common.utils import exc_type_msg
+from bos.common.utils import exc_type_msg, requests_retry_session
 from bos.operators.utils.boot_image_metadata import BootImageMetaData
 from bos.operators.utils.boot_image_metadata.factory import BootImageMetaDataFactory
-from bos.operators.utils.clients.ims import ImageNotFound
+from bos.operators.utils.clients.ims import get_arch_from_image_data, get_image, \
+                                            get_ims_id_from_s3_url, ImageNotFound
 from bos.operators.utils.clients.s3 import S3Object, ArtifactNotFound
 from bos.server.controllers.v2.options import OptionsData
 from bos.server.utils import canonize_xname, ParsingException
@@ -58,13 +59,25 @@ class BootSetError(Exception):
     """
 
 
+class BootSetWarning(Exception):
+    """
+    Generic error class for non-fatal problems found during boot set validation
+    """
+
+
+class NonImsImage(BootSetWarning):
+    """
+    Raised to indicate the boot set boot image is not from IMS
+    """
+
+
 class BootSetArchMismatch(BootSetError):
     def __init__(self, bs_arch: str, expected_ims_arch: str, actual_ims_arch: str):
         super().__init__(f"Boot set arch '{bs_arch}' means IMS image arch should be "
                          f"'{expected_ims_arch}', but actual IMS image arch is '{actual_ims_arch}'")
 
 
-class CannotValidateBootSetArch(BootSetError):
+class CannotValidateBootSetArch(BootSetWarning):
     def __init__(self, msg: str):
         super().__init__(f"Can't validate boot image arch: {msg}")
 
@@ -159,18 +172,14 @@ def _validate_boot_set(bs: dict, operation: str, options_data: OptionsData) -> l
     if operation in ['boot', 'reboot']:
         # Verify that the boot artifacts exist
         try:
-            image_metadata = BootImageMetaDataFactory(bs, options_data)()
+            image_metadata = BootImageMetaDataFactory(bs)()
         except Exception as err:
             raise BootSetError(f"Can't find boot artifacts. Error: {exc_type_msg(err)}") from err
 
         try:
-            validate_boot_set_arch(bs, image_metadata)
-        except CannotValidateBootSetArch as err:
+            validate_ims_boot_image(bs, image_metadata, options_data)
+        except BootSetWarning as err:
             warning_msgs.append(str(err))
-        except BootSetError as err:
-            raise BootSetError(f"Arch validation error: {err}") from err
-        except Exception as err:
-            raise BootSetError(f"Arch validation error: {exc_type_msg(err)}") from err
 
         # Check boot artifacts' S3 headers
         for boot_artifact in ["kernel"]:
@@ -205,25 +214,51 @@ def _validate_boot_set(bs: dict, operation: str, options_data: OptionsData) -> l
     return warning_msgs
 
 
-def validate_boot_set_arch(bs: dict, image_metadata: BootImageMetaData) -> None:
+def validate_ims_boot_image(bs: dict, image_metadata: BootImageMetaData,
+                            options_data: OptionsData) -> None:
     """
     If the boot set architecture is not set to Other, check that the IMS image
     architecture matches the boot set architecture (treating a boot set architecture
     of Unknown as X86)
+
+    Otherwise, at least validate whether the boot image is in IMS, if we expect it to be.
     """
-    arch = bs.get("arch", DEFAULT_ARCH)
-    if arch == 'Other':
+    bs_arch = bs.get("arch", DEFAULT_ARCH)
+
+    ims_id = get_ims_image_id(image_metadata)
+
+    # If IMS being inaccessible is not a fatal error, then reduce the number
+    # of retries we make, to prevent a lengthy delay
+    num_retries = None if options_data.ims_errors_fatal else 4
+
+    try:
+        image_data = get_ims_image_data(ims_id, num_retries)
+    except ImageNotFound as err:
+        raise BootSetWarning(str(err)) from err
+    except Exception as err:
+        if options_data.ims_errors_fatal:
+            raise err
+        if bs_arch != 'Other':
+            # This means that this error is preventing us from validating the
+            # boot set architecture
+            raise CannotValidateBootSetArch(str(err)) from err
+        # We weren't going to be validating the architecture, since it is Other,
+        # but we should still log this as a warning
+        raise BootSetWarning(str(err)) from err
+
+    if bs_arch == 'Other':
         raise CannotValidateBootSetArch("Boot set arch set to 'Other'")
 
     try:
-        ims_image_arch = image_metadata.arch
-    except ImageNotFound as err:
-        raise CannotValidateBootSetArch(str(err)) from err
+        ims_image_arch = get_arch_from_image_data(image_data)
+    except Exception as err:
+        # This most likely indicates that the IMS image data we got wasn't even a dict
+        if options_data.ims_errors_fatal:
+            raise err
+        raise BootSetWarning(str(err)) from err
 
-    if ims_image_arch is None:
-        raise CannotValidateBootSetArch("Can't determine architecture of boot artifacts")
-    if EXPECTED_IMS_ARCH[arch] != ims_image_arch:
-        raise BootSetArchMismatch(bs_arch=arch, expected_ims_arch=EXPECTED_IMS_ARCH[arch],
+    if EXPECTED_IMS_ARCH[bs_arch] != ims_image_arch:
+        raise BootSetArchMismatch(bs_arch=bs_arch, expected_ims_arch=EXPECTED_IMS_ARCH[bs_arch],
                                   actual_ims_arch=ims_image_arch)
 
 
@@ -272,19 +307,18 @@ def validate_sanitize_boot_set(bs_name: str, bs_data: dict, options_data: Option
 
     # Get the boot image metadata
     try:
-        image_metadata = BootImageMetaDataFactory(bs_data, options_data)()
+        image_metadata = BootImageMetaDataFactory(bs_data)()
     except Exception as err:
         LOGGER.warning("Can't find boot artifacts: %s", exc_type_msg(err))
     else: # No exception was raised in try block
-        # Validate the boot set architecture
+        # Validate the boot set IMS image
         try:
-            validate_boot_set_arch(bs_data, image_metadata)
-        except CannotValidateBootSetArch as err:
-            LOGGER.warning('%s', bs_data)
+            validate_ims_boot_image(bs_data, image_metadata, options_data)
+        except BootSetWarning as err:
             LOGGER.warning("Boot set '%s': %s", bs_name, err)
+            LOGGER.warning('Boot set contents: %s', bs_data)
         except Exception as err:
-            raise ParsingException(f"Error found validating arch of boot set '{bs_name}': " \
-                                   f"{exc_type_msg(err)}") from err
+            raise ParsingException(str(err)) from err
 
     # Validate that the boot set has at least one of the HARDWARE_SPECIFIER_FIELDS
     if not any(field_name in bs_data for field_name in HARDWARE_SPECIFIER_FIELDS):
@@ -323,3 +357,31 @@ def validate_sanitize_boot_set(bs_name: str, bs_data: dict, options_data: Option
 
     # Update the node_list value in the boot set with the canonized version
     bs_data["node_list"] = new_node_list
+
+
+def get_ims_image_id(image_metadata: BootImageMetaData) -> str:
+    """
+    If the image is an IMS image, return its ID.
+    Raise NonImsImage otherwise,
+    Note that this does not actually check IMS to see if the ID
+    exists.
+    """
+    s3_url = image_metadata.manifest_s3_url
+    ims_id = get_ims_id_from_s3_url(s3_url)
+    if ims_id:
+        return ims_id
+    raise NonImsImage(f"Boot artifact S3 URL '{s3_url.url}' doesn't follow convention "
+                      "for IMS images")
+
+
+def get_ims_image_data(ims_id: str, num_retries: int|None=None) -> dict:
+    """
+    Query IMS to get the image data and return it,
+    or raise an exception.
+    """
+    kwargs = { "image_id": ims_id }
+    if num_retries is not None:
+        # A pylint bug generates a false positive error for this call
+        # https://github.com/pylint-dev/pylint/issues/2271
+        kwargs['session'] = requests_retry_session(retries=4) # pylint: disable=redundant-keyword-arg
+    return get_image(**kwargs)
