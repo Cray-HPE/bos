3f2ad4c1c411093053cfd235453747c40f578d23
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2025-01-06 13:26:27 -0500

    fix

--- src/bos/server/controllers/v2/components.py
+++ src/bos/server/controllers/v2/components.py
@@ -61,7 +61,7 @@ def get_v2_components(ids=None, enabled=None, session=None, staged_session=None,
                 detail=str(err))
     else:
         id_list = None
-    tenant = get_tenant_from_header()
+    tenant = get_tenant_from_header() or None
     LOGGER.debug("GET /v2/components for tenant=%s with %d IDs specified",
                  tenant, len(id_list) if id_list else 0)
     response = get_v2_components_data(id_list=id_list, enabled=enabled, session=session,
