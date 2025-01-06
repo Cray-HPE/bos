1eec24a374f1dae586e97825a87eca052190f8f1
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2025-01-06 14:07:59 -0500

    Fixed bug causing no components to be listed when no tenant specified

--- src/bos/server/controllers/v2/components.py
+++ src/bos/server/controllers/v2/components.py
@@ -42,7 +42,7 @@ SESSIONS_DB = dbutils.get_wrapper(db='sessions')
 
 @tenant_error_handler
 @dbutils.redis_error_handler
-def get_v2_components(ids="", enabled=None, session=None, staged_session=None, phase=None,
+def get_v2_components(ids=None, enabled=None, session=None, staged_session=None, phase=None,
                       status=None):
     """Used by the GET /components API operation
 
@@ -51,7 +51,7 @@ def get_v2_components(ids="", enabled=None, session=None, staged_session=None, p
     LOGGER.debug("GET /v2/components invoked get_v2_components with ids=%s enabled=%s session=%s "
                  "staged_session=%s phase=%s status=%s", ids, enabled, session, staged_session,
                  phase, status)
-    if ids:
+    if ids is not None:
         try:
             id_list = ids.split(',')
         except Exception as err:
@@ -61,7 +61,7 @@ def get_v2_components(ids="", enabled=None, session=None, staged_session=None, p
                 detail=str(err))
     else:
         id_list = None
-    tenant = get_tenant_from_header()
+    tenant = get_tenant_from_header() or None
     LOGGER.debug("GET /v2/components for tenant=%s with %d IDs specified",
                  tenant, len(id_list) if id_list else 0)
     response = get_v2_components_data(id_list=id_list, enabled=enabled, session=session,
