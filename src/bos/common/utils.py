f4ae2ca99239b5236b01b7112a08b570cca1fb41
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2025-04-14 13:42:00 -0400

    Alter bos.common.utils to avoid syntax error when imported by bos reporter
    (cherry picked from commit 97c220901d32b3f7cf6c7fb20be2ed8de960837b)

--- src/bos/common/utils.py
+++ src/bos/common/utils.py
@@ -24,6 +24,8 @@
 import datetime
 import re
 import traceback
+from typing import Optional
+
 from dateutil.parser import parse
 
 TIME_DURATION_PATTERN = re.compile("^(\d+?)(\D+?)$", re.M|re.S)
@@ -68,7 +70,7 @@ class compact_response_text:
     """
     _SPLIT_PAT = re.compile(r'([^\n]+)(?:$|\n)')
 
-    def __init__(self, response_text: str | None) -> None:
+    def __init__(self, response_text: Optional[str]) -> None:
         self._response_text = response_text
 
     @property
